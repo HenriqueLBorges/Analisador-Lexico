acf30aa639d4e7618475c0f1756b7906cdeba786
Henrique Lima Borges - TI <hlborges@bssp.local>
2017-03-08 10:11:37 -0300

    Inclusão de linhas e blocos de comentarios

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,24 @@
 
 public class Main {
 	public static void main(String Args []){
-		String linha = "ALGorITMO";
+		String linha = "\\";
+		linha += '"';
+		linha += "ouhui636ey8732gb ydsvc f  ^ ^ `";
+		linha += '"';
 		Idendificador palavra = new Idendificador();
+		Frase frase = new Frase();
 		Numero_Real numero = new Numero_Real();
 		System.out.println(palavra.setAtomo(linha));
 		System.out.println(palavra.getAtomo());
 		System.out.println("---------------------------------");
 		System.out.println(numero.setAtomo(linha));
 		System.out.println(numero.getAtomo());
+		System.out.println("---------------------------------");
+		System.out.println(frase.setAtomo(linha));
+		System.out.println(frase.getAtomo());
+		linha = "/* ouhui636ey8732gb ydsvc f  ^ ^ `  */";
+		Comentario comentario = new Comentario();
+		System.out.println(comentario.setAtomo(linha));
+		System.out.println(comentario.getAtomo());
 	}
 }
