1d26d59dbbe6041b7158cf5d38bf89702d5a1faa
Henrique Lima Borges - TI <hlborges@bssp.local>
2017-03-18 08:44:30 -0300

    Detecção de atomos sem atributos corrigida

--- src/AtomoSemAtributo.java
+++ src/AtomoSemAtributo.java
@@ -4,20 +4,23 @@ public class AtomoSemAtributo extends Atomo {
 	@Override
 	public boolean setAtomo(String atomo) {
 		// TODO Auto-generated method stub
-		for (int i = 0; i < atomo.length(); i++) {
-			if (verificaAtomoSemAtributo(atomo.charAt(i))) {
-				this.atomoArmazenado = atomo;
-				return true;
-			}
-		}
+		boolean verifica = false;
+		if (verificaAtomoSemAtributo(atomo.charAt(0)) && atomo.length() < 2) {
+			this.atomoArmazenado = "" + atomo.charAt(0);
+			verifica = true;
+		} else
+			verifica = false;
+		if (verifica)
+			return true;
 		return false;
 	}
 
 	private boolean verificaAtomoSemAtributo(char caractere) {
 		// 40 = '(', 41 = ')', 45 = '-', 46 = '.', 47 = '/' e 59 = ';'
 		if (caractere == 40 || caractere == 41 || caractere == 45 || caractere == 46 || caractere == 47
-				|| caractere == 59)
+				|| caractere == 59) {
 			return true;
+		}
 		return false;
 	}
 }
